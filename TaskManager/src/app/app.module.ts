23bb7034775ddd95bbe57d5f1e53a26d3d685c67
Jae No <jaeyno@gmail.com>
2021-07-13 06:42:09 -0400

    [About UI] create about component

--- TaskManager/src/app/app.module.ts
+++ TaskManager/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { AboutComponent } from './about/about.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    DashboardComponent
+    DashboardComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,

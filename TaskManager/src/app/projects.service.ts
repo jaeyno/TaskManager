340aa7226aa2e04d6af16cbdc85909fc46e12a8c
Jae No <jaeyno@gmail.com>
2021-07-17 16:36:17 -0400

    [TaskManager UI] added projects components

--- TaskManager/src/app/projects.service.ts
+++ TaskManager/src/app/projects.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Project } from './project';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProjectsService {
 
-  constructor() { }
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  getAllProjects(): Observable<Project[]> {
+    return this.http.get<Project[]>("/api/projects");
+  }
 }
